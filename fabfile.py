4032f7c5844de7b4747f2e91aa6213befc0d7eb4
drytuna <drytuna@gmail.com>
2014-06-18 15:09:49 -0700

    host_type, get_conn, prov_instances, list_instances

---
+++ fabfile.py
@@ -0,0 +1,72 @@
+from fabric.api import run
+from fabric.api import env
+import boto.ec2
+import time
+
+env.hosts = ['localhost', ]
+env.aws_region = 'us-west-2'
+
+def host_type():
+    run('uname -s')
+
+def get_ec2_connection():
+    if 'ec2' not in env:
+        conn = boto.ec2.connect_to_region(env.aws_region)
+        if conn is not None:
+            env.ec2 = conn
+            print "Connected to EC2 region %s" % env.aws_region
+        else:
+            msg = "Unable to connect to EC2 region %s"
+            raise IOError(msg % env.aws_region)
+    return env.ec2
+
+def provision_instance(wait_for_running=False, timeout=60, interval=2):
+    wait_val = int(interval)
+    timeout_val = int(timeout)
+    conn = get_ec2_connection()
+    instance_type = 't1.micro'
+    key_name = 'mykeypair'
+    security_group = 'ssh-access'
+    image_id = 'ami-bf582a8f'
+
+    reservations = conn.run_instances(
+        image_id,
+        key_name=key_name,
+        instance_type=instance_type,
+        security_groups=[security_group, ]
+    )
+    new_instances = [i for i in reservations.instances if i.state == u'pending']
+    running_instance = []
+    if wait_for_running:
+        waited = 0
+        while new_instances and (waited < timeout_val):
+            time.sleep(wait_val)
+            waited += int(wait_val)
+            for instance in new_instances:
+                state = instance.state
+                print "Instance %s is %s" % (instance.id, state)
+                if state == "running":
+                    running_instance.append(
+                        new_instances.pop(new_instances.index(i))
+                    )
+                instance.update()
+
+def list_aws_instances(verbose=False, state='all'):
+    conn = get_ec2_connection()
+    reservations = conn.get_all_reservations()
+    instances = []
+    for res in reservations:
+        for instance in res.instances:
+            if state == 'all' or instance.state == state:
+                instance = {
+                    'id': instance.id,
+                    'type': instance.instance_type,
+                    'image': instance.image_id,
+                    'state': instance.state,
+                    'instance': instance,
+                }
+                instances.append(instance)
+    env.instances = instances
+    if verbose:
+        import pprint
+        pprint.pprint(env.instances)
